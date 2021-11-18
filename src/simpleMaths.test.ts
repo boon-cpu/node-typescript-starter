615fcb68a2ec4ecd6aadf9ecb908a9728a6997a0
boon-cpu <harrisonboon@gmail.com>
2021-11-18 09:21:27 +0000

    added function + test + prettierised :sparkles: :white_check_mark: :recycle:

--- src/simpleMaths.test.ts
+++ src/simpleMaths.test.ts
@@ -1,9 +1,17 @@
-import { calcAreaSquare, calcAreaTriangle } from './simpleMaths'
+import {
+  calcAreaSquare,
+  calcAreaTriangle,
+  calcPerimeterSquare,
+} from "./simpleMaths";
 
-test('Area Square', () => {
-    expect(calcAreaSquare(4)).toBe(16);
-})
+test("Area Square", () => {
+  expect(calcAreaSquare(4)).toBe(16);
+});
 
-test('Area Triangle', () => {
-    expect(calcAreaTriangle(4, 5)).toBe(10);
-})
+test("Area Triangle", () => {
+  expect(calcAreaTriangle(4, 5)).toBe(10);
+});
+
+test("Surface area of square", () => {
+  expect(calcPerimeterSquare(4)).toBe(16);
+});
